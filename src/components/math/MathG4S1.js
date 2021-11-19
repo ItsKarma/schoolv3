06cab496c512509dbdec7fbf02b29716e9980040
Matt Karmazyn <matt@itskarma.wtf>
2021-11-19 16:58:15 -0500

    refactor

--- src/components/math/MathG4S1.js
+++ src/components/math/MathG4S1.js
@@ -1,7 +1,8 @@
-const F1 = () => {
-  const names = ["Kristen", "Matt", "Gracie", "Connor", "Sally", "Max", "Cici"];
-  const foods = ["hot dogs", "apples", "peachs", "tacos"];
+const names = ["Kristen", "Matt", "Gracie", "Connor", "Sally", "Max", "Cici"];
+const foods = ["hot dogs", "apples", "peachs", "tacos"];
+const items = ["apples", "bananas", "peachs", "tacos"];
 
+const F1 = () => {
   const name1 = names[Math.floor(Math.random()*names.length)];
   const food = foods[Math.floor(Math.random()*foods.length)];
 
@@ -25,30 +26,27 @@ const F1 = () => {
 
 
 const F2 = () => {
-    const names = ["Kristen", "Matt", "Gracie", "Connor", "Sally", "Max", "Cici"];
-    const items = ["apples", "bananas", "peachs", "tacos"];
-  
-    const name1 = names[Math.floor(Math.random()*names.length)];
-    const item = items[Math.floor(Math.random()*items.length)];
-  
-    // get position of name1 and remove it from the list so name2 is different.
-    const index = names.indexOf(name1);
-    if (index > -1) {
-      names.splice(index, 1);
-    }
-  
-    const name2 = names[Math.floor(Math.random()*names.length)];
-  
-    const num1 = Math.floor(Math.random() * 10) + 1;
-    const value1 = Math.floor(Math.random() * 10) + 1;
-    const num2 = num1 + Math.floor(Math.random() * 10) + 1;
-    const value2 = value1 + Math.floor(Math.random() * 10) + 1;
-  
-    return {
-      question: `${name1} sold ${num1} ${item} for $${value1} each. ${name2} sold ${num2} ${item} for $${value2} each. How much more money did ${name2} make than ${name1}?`,
-      answer: (num2 * value2) - (num1 * value1),
-      formula: `(${num2} * ${value2}) - (${num1} * ${value1})`
-    }
+  const name1 = names[Math.floor(Math.random()*names.length)];
+  const item = items[Math.floor(Math.random()*items.length)];
+
+  // get position of name1 and remove it from the list so name2 is different.
+  const index = names.indexOf(name1);
+  if (index > -1) {
+    names.splice(index, 1);
+  }
+
+  const name2 = names[Math.floor(Math.random()*names.length)];
+
+  const num1 = Math.floor(Math.random() * 10) + 1;
+  const value1 = Math.floor(Math.random() * 10) + 1;
+  const num2 = num1 + Math.floor(Math.random() * 10) + 1;
+  const value2 = value1 + Math.floor(Math.random() * 10) + 1;
+
+  return {
+    question: `${name1} sold ${num1} ${item} for $${value1} each. ${name2} sold ${num2} ${item} for $${value2} each. How much more money did ${name2} make than ${name1}?`,
+    answer: (num2 * value2) - (num1 * value1),
+    formula: `(${num2} * ${value2}) - (${num1} * ${value1})`
+  }
 }
 
 const MathG4S1 = {
