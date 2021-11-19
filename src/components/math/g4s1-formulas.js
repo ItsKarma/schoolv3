88a1645ea1b595fc1530a2b3dcd5aa20a002d8f3
Matt Karmazyn <matt@itskarma.wtf>
2021-11-19 08:38:16 -0500

    moving formula

--- src/components/math/g4s1-formulas.js
+++ src/components/math/g4s1-formulas.js
@@ -1,14 +1,24 @@
-export default function Formulas() {
-  const names = ["Kristen", "Matt", "Gracie", "Connor", "Sally", "Max", "Cici"]
+export default function G4S1Formulas () {
+  const names = ["Kristen", "Matt", "Gracie", "Connor", "Sally", "Max", "Cici"];
+  const foods = ["hot dogs", "apples", "peachs", "tacos"];
 
-  const name1 = names[Math.floor(Math.random()*names.length-1)];
-  const name2 = names[Math.floor(Math.random()*names.length-1)];
+  const name1 = names[Math.floor(Math.random()*names.length)];
+  const food = foods[Math.floor(Math.random()*foods.length)];
 
-  const num1 = Math.floor(Math.random() * 10);
-  const num2 = Math.floor(Math.random() * 10);
+  // get position of name1 and remove it from the list so name2 is different.
+  const index = names.indexOf(name1);
+  if (index > -1) {
+    names.splice(index, 1);
+  }
+
+  const name2 = names[Math.floor(Math.random()*names.length)];
+
+  const num1 = Math.floor(Math.random() * 10) + 1;
+  const num2 = num1 + Math.floor(Math.random() * 10) + 1;
 
   return {
-      question: `${name1} ate ${num1} hot dogs. ${name2} ate ${num2} hot dogs. How many more hot dogs did ${name2} eat than ${name1}?`,
-      answer: name2 - name1
+    question: `${name1} ate ${num1} ${food}. ${name2} ate ${num2} ${food}. How many more ${food} did ${name2} eat than ${name1}?`,
+    answer: num2 - num1,
+    formula: `${num2} - ${num1}`
   }
 }
