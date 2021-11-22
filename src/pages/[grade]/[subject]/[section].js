bf83324a4a425291555d11f72226dc46005ef181
Matt Karmazyn <matt@itskarma.wtf>
2021-11-22 09:51:29 -0500

    cleanup question loading

--- src/pages/[grade]/[subject]/[section].js
+++ src/pages/[grade]/[subject]/[section].js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import GetQuestion from '../../../components/getQuestion';
 import Header from '../../../components/header';
@@ -6,39 +5,14 @@ import Footer from '../../../components/footer';
 import styles from '../../../styles/Home.module.css';
 
 export default function SectionPage () {
-  const [_subject, setSubject] = useState("");
-  const [_grade, setGrade] = useState("");
-  const [_section, setSection] = useState("");
-  const [niceSubject, setNiceSubject] = useState("");
+  const router = useRouter();
+  const { grade, subject, section } = router.query;
 
-  const router = useRouter()
-  useEffect(()=>{
-    if(!router.isReady) return;
-    const { grade, subject, section } = router.query;
-    setGrade(grade);
-    setSubject(subject);
-    setSection(section);
-    // Uppercase the first character of the subject.
-    setNiceSubject(`${subject}`.charAt(0).toUpperCase() + `${subject}`.slice(1));
-  }, [router.isReady]);
-
-  const createQuestion = () => {
-    let form = {question: "Loading Question...", formula: "Loading Formula...", answer: "Loading Answer..."}
-    try {
-      form = GetQuestion(_grade, _subject, _section);
-    } catch {
-      // Commented out to clean console in prod.
-      // console.log("Unable to load question.")
-    }
-    return (
-      <div>
-        <p>Question: {form.question}</p>
-        <details>
-          <summary>Answer: </summary>
-          <p>{form.formula} = {form.answer}</p>
-        </details>
-      </div>
-    )
+  let form = {question: "Loading Question...", formula: "Loading Formula...", answer: "Loading Answer..."}
+  let niceSubject = "";
+  if (router.isReady) {
+    form = GetQuestion(grade, subject, section);
+    niceSubject = `${subject}`.charAt(0).toUpperCase() + `${subject}`.slice(1);
   }
 
   return (
@@ -47,14 +21,20 @@ export default function SectionPage () {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Grade {_grade} - {niceSubject}
+          Grade {grade} - {niceSubject}
         </h1>
 
         <p className={styles.description}>
-          Section {_section}
+          Section {section}
         </p>
 
-        {createQuestion()}
+        <div>
+          <p>Question: {form.question}</p>
+          <details>
+            <summary>Answer: </summary>
+            <p>{form.formula} = {form.answer}</p>
+          </details>
+        </div>
 
       </main>
       <Footer />
