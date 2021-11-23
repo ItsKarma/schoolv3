3108e7c059a0809e0c070f56b98f6ab2080ac9df
Matt Karmazyn <matt@itskarma.wtf>
2021-11-23 10:49:27 -0500

    word problem common utils

--- src/components/curriculum/4/math/1.1.js
+++ src/components/curriculum/4/math/1.1.js
@@ -1,46 +1,28 @@
-import { names, foods, items } from '../../../utils';
+import { getRandomNames, foods, items } from '../../../utils';
 
 const F1 = () => {
-  const name1 = names[Math.floor(Math.random()*names.length)];
+  const randomNames = getRandomNames();
   const food = foods[Math.floor(Math.random()*foods.length)];
-
-  // get position of name1 and remove it from the list so name2 is different.
-  const index = names.indexOf(name1);
-  if (index > -1) {
-    names.splice(index, 1);
-  }
-
-  const name2 = names[Math.floor(Math.random()*names.length)];
-
   const num1 = Math.floor(Math.random() * 10) + 1;
   const num2 = num1 + Math.floor(Math.random() * 10) + 1;
 
   return {
-    question: `${name1} ate ${num1} ${food}. ${name2} ate ${num2} ${food}. How many more ${food} did ${name2} eat than ${name1}?`,
+    question: `${randomNames[0]} ate ${num1} ${food}. ${randomNames[1]} ate ${num2} ${food}. How many more ${food} did ${randomNames[1]} eat than ${randomNames[0]}?`,
     answer: num2 - num1,
     formula: `${num2} - ${num1}`
   }
 }
 
 const F2 = () => {
-  const name1 = names[Math.floor(Math.random()*names.length)];
+  const randomNames = getRandomNames();
   const item = items[Math.floor(Math.random()*items.length)];
-
-  // get position of name1 and remove it from the list so name2 is different.
-  const index = names.indexOf(name1);
-  if (index > -1) {
-    names.splice(index, 1);
-  }
-
-  const name2 = names[Math.floor(Math.random()*names.length)];
-
   const num1 = Math.floor(Math.random() * 10) + 1;
   const value1 = Math.floor(Math.random() * 10) + 1;
   const num2 = num1 + Math.floor(Math.random() * 10) + 1;
   const value2 = value1 + Math.floor(Math.random() * 10) + 1;
 
   return {
-    question: `${name1} sold ${num1} ${item} for $${value1} each. ${name2} sold ${num2} ${item} for $${value2} each. How much more money did ${name2} make than ${name1}?`,
+    question: `${randomNames[0]} sold ${num1} ${item} for $${value1} each. ${randomNames[1]} sold ${num2} ${item} for $${value2} each. How much more money did ${randomNames[1]} make than ${randomNames[0]}?`,
     answer: (num2 * value2) - (num1 * value1),
     formula: `(${num2} x ${value2}) - (${num1} x ${value1})`
   }
--- src/components/utils.js
+++ src/components/utils.js
@@ -2,8 +2,29 @@ const names = ["Kristen", "Matt", "Gracie", "Connor", "Sally", "Max", "Cici"]
 const foods = ["hot dogs", "apples", "peachs", "tacos"];
 const items = ["watches", "chairs", "tables", "pictures"];
 
+const getRandomNames = () => {
+  // Returns 3 random names as a list.
+  const name1 = names[Math.floor(Math.random()*names.length)];
+  // Get position of name1 and remove it from the array so name2 is different.
+  // TODO: Turn this into a separate function so we can use recursion and pass in number of names for us to retrieve.
+  const index = names.indexOf(name1);
+  if (index > -1) {
+    names.splice(index, 1);
+  }
+  const name2 = names[Math.floor(Math.random()*names.length)];
+
+  // Get position of name2 and remove it from the array so name3 is different.
+  const index2 = names.indexOf(name2);
+  if (index2 > -1) {
+    names.splice(index2, 1);
+  }
+  const name3 = names[Math.floor(Math.random()*names.length)];
+
+  return [name1, name2, name3]
+}
+
 const Utils = {
-  names, foods, items
+  names, getRandomNames, foods, items
 }
   
 module.exports = Utils
