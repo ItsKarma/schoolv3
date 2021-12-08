53dbcbe5dbde4cb70d7c7c510a412078c52062f2
Matt Karmazyn <matt@itskarma.wtf>
2021-12-08 06:58:00 -0500

    typo fix

--- src/components/curriculum/4/math/1.2.js
+++ src/components/curriculum/4/math/1.2.js
@@ -9,7 +9,7 @@ const F1 = () => {
   const value2 = randomInt(13, 99);
 
   return {
-    question: `${randomNames[0]} sold ${num1} ${item} for $${value1} each. ${randomNames[1]} sold ${num2} ${item} for $${value2} each. How money did ${randomNames[0]} and ${randomNames[1]} make all together?`,
+    question: `${randomNames[0]} sold ${num1} ${item} for $${value1} each. ${randomNames[1]} sold ${num2} ${item} for $${value2} each. How much money did ${randomNames[0]} and ${randomNames[1]} make all together?`,
     answer: (num2 * value2) - (num1 * value1),
     formula: `(${num2} x ${value2}) + (${num1} x ${value1})`,
     explanation: `all together means we want to add.`
